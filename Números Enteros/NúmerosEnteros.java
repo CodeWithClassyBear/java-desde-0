e905c2d089df687b290f48616b3007eb47978ea4
Classy-Bear <davidfco1702@protonmail.com>
2021-01-09 11:57:24 -0400

    Spaces over tabs

--- Números Enteros/NúmerosEnteros.java
+++ Números Enteros/NúmerosEnteros.java
@@ -5,30 +5,30 @@
 */
 class NúmerosEnteros {
 	public static void main(String[] args) {
-		/*
-		 * Ya que vimos muchos números por hoy, ¿Por qué no hacemos operaciones
-		 * matemáticas? No... ah solo ¿yo? Bueno esta bien escribamos un poema...
-		 */
-		// Estas son variables con su tipo de dato.
-		byte edad = 20;
-		short añoActual = 2021;
-		int númerosAlAzar = 2910121;
-		long númerosAlAzarRepetido = 29101212910121L;
-		/*
-		 * Tal vez te preguntas porque no hay un "Para saber más…" en el capítulo
-		 * de los números enteros, bueno eso es porque estará aquí, aprenderemos
-		 * como concatenar variables.
-		 */
-		System.out.println("Esto es " + "concatenar"); // Imprime: Esto es concatenar
-		System.out.println(); // Salto de línea.
-		// Aquí esta mi poema.
-		System.out.println("Tengo " + edad + " años.");
-		System.out.println("Este tutorial fue escrito en " + añoActual + ".");
-		System.out.println("Conté hasta " + númerosAlAzar + " y no me quedé dormido.");
-		System.out.println("Así que conté " + númerosAlAzarRepetido + " obejas y ... me quedé dormido.");
-		System.out.println(); // Salto de línea.
-		// Aquí escribe tu poema.
-		// ...
+    /*
+     * Ya que vimos muchos números por hoy, ¿Por qué no hacemos operaciones
+     * matemáticas? No... ah solo ¿yo? Bueno esta bien escribamos un poema...
+     */
+    // Estas son variables con su tipo de dato.
+    byte edad = 20;
+    short añoActual = 2021;
+    int númerosAlAzar = 2910121;
+    long númerosAlAzarRepetido = 29101212910121L;
+     /*
+     * Tal vez te preguntas porque no hay un "Para saber más…" en el capítulo
+     * de los números enteros, bueno eso es porque estará aquí, aprenderemos
+     * como concatenar variables.
+     */
+    System.out.println("Esto es " + "concatenar"); // Imprime: Esto es concatenar
+    System.out.println(); // Salto de línea.
+    // Aquí esta mi poema.
+    System.out.println("Tengo " + edad + " años.");
+    System.out.println("Este tutorial fue escrito en " + añoActual + ".");
+    System.out.println("Conté hasta " + númerosAlAzar + " y no me quedé dormido.");
+    System.out.println("Así que conté " + númerosAlAzarRepetido + " obejas y ... me quedé dormido.");
+    System.out.println(); // Salto de línea.
+    // Aquí escribe tu poema.
+    // ...
 	}
 }
 
